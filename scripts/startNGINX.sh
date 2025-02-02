4b62ce52e048bad120bfb15b7f93b0c62fef1b94
mohammed eissa7 <smme012345@gmail.com>
2025-02-02 19:31:06 +0000

    #1 NGINX install

---
+++ scripts/startNGINX.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+sudo apt update
+
+
+sudo apt install -y nginx
+
+cd /var/www
+
+if [ ! -e /var/www/tutorial ]; then
+    mkdir tutorial
+fi
+
+cd tutorial
+
+if [ ! -e /var/www/tutorial/index.html ]; then
+    touch  index.html <<EOf
+    <!doctype html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>Hello, Nginx!</title>
+    </head>
+    <body>
+        <h1>Hello, Nginx!</h1>
+        <p>We have just configured our Nginx web server on Ubuntu Server!</p>
+    </body>
+    </html>
+EOf
+fi
+
+
+
+cd /etc/nginx/sites-enabled
+
+
+if [ ! -e /var/www/tutorial ]; then
+    touch  tutorial <<EOf
+server {
+       listen 8080;
+       listen [::]:8080;
+
+       server_name example.ubuntu.com;
+
+       root /var/www/tutorial;
+       index index.html;
+
+       location / {
+               try_files $uri $uri/ =404;
+       }
+}
+EOf
+fi
+
+sudo systemctl restart nginx
